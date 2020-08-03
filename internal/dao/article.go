bbe28d6264877f6f13eb4593c07924f3ccd3cdcb
xyl <416319808@qq.com>
2020-08-03 18:21:45 +0800

    文章curd

--- internal/dao/article.go
+++ internal/dao/article.go
@@ -2,14 +2,15 @@ package dao
 
 import (
 	"blog/init/base"
+	"blog/internal/model"
 	"github.com/jinzhu/gorm"
 )
 
 type ArticleDao interface {
-	Create()
-	Select()
-	Update()
-	Delete()
+	Select(pageNum, pageSize uint, maps interface{}) (articles []model.Article, total int64, err error)
+	Create(article2 *model.Article) (id uint, err error)
+	Update(article2 *model.Article) (ok bool)
+	Delete(id uint) bool
 }
 
 func NewArticleDao() ArticleDao {
@@ -22,18 +23,23 @@ type article struct {
 	db *gorm.DB
 }
 
-func (a *article) Create() {
-
+// Create 创建文章
+func (a *article) Create(article2 *model.Article) (id uint, err error) {
+	err = a.db.Create(article2).Error
+	return article2.ID, err
 }
 
-func (a *article) Delete() {
-
+// Delete 删除文章
+func (a *article) Delete(id uint) bool {
+	return a.db.Where("id = ?", id).Delete(&model.Article{}).RowsAffected > 0
 }
 
-func (a *article) Update() {
-
+// Update 更新文章
+func (a *article) Update(article2 *model.Article) (ok bool) {
+	return a.db.Save(article2).RowsAffected > 0
 }
 
-func (a *article) Select() {
-
+func (a *article) Select(pageNum, pageSize uint, maps interface{}) (articles []model.Article, total int64, err error) {
+	err = a.db.Where(maps).Offset(pageNum).Limit(pageSize).Find(&articles).Count(&total).Error
+	return
 }
---
+++ internal/dao/article_test.go
@@ -0,0 +1,73 @@
+package dao
+
+import (
+	"blog/internal/model"
+	"blog/tools"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestArticle_Create(t *testing.T) {
+	Convey("创建文章", t, func() {
+		dao := NewArticleDao()
+		article := model.Article{
+			CategoryId: 1,
+			TagId:      1,
+			Title:      "test create",
+			Content:    "this is a test",
+			Visits:     1,
+		}
+		id, err := dao.Create(&article)
+		So(err, ShouldBeNil)
+		So(id, ShouldBeGreaterThan, 0)
+		t.Log(id)
+	})
+}
+
+func TestArticle_Delete(t *testing.T) {
+	Convey("删除文章", t, func() {
+		dao := NewArticleDao()
+		article := &model.Article{
+			CategoryId: 1,
+			TagId:      1,
+			Title:      "delete" + tools.RandomString(10),
+			Content:    "testing for delete article",
+			Visits:     1,
+		}
+		id, err := dao.Create(article)
+		So(err, ShouldBeNil)
+		So(id, ShouldBeGreaterThan, 0)
+		ok := dao.Delete(id)
+		So(ok, ShouldBeTrue)
+	})
+}
+
+func TestArticle_Update(t *testing.T) {
+	Convey("更新文章", t, func() {
+		dao := NewArticleDao()
+		article := &model.Article{
+			CategoryId: 1,
+			TagId:      1,
+			Title:      "update-" + tools.RandomString(10),
+			Content:    "testing for update article",
+			Visits:     1,
+		}
+		id, err := dao.Create(article)
+		So(err, ShouldBeNil)
+		So(id, ShouldBeGreaterThan, 0)
+		article.Title = article.Title + tools.RandomNumber(4)
+		article.Content = article.Content + " - " + tools.RandomString(50)
+		ok := dao.Update(article)
+		So(ok, ShouldBeTrue)
+	})
+}
+
+func TestArticle_Select(t *testing.T) {
+	Convey("查询文章", t, func() {
+		dao := NewArticleDao()
+		_, total, err := dao.Select(0, 10, "")
+		So(err, ShouldBeNil)
+		So(total, ShouldBeGreaterThanOrEqualTo, 0)
+		t.Logf("count:%d", total)
+	})
+}
